5ee09ac0542358c3a6fd534a7d54636829b15df8
root <root@localhost.localdomain>
2015-10-11 13:46:19 -0400

    parse access log text

--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -17,33 +17,68 @@ def nmap_scan? (pkt)
        return ((pkt.payload).include? "nmap")
 end
 
-
-stream = PacketFu::Capture.new(:start => true, :iface => 'eth0', :promisc => true)
-
 def alert (desc, pkt, i)
-	puts "#{i}. ALERT: #{desc} for #{pkt.ip_saddr} (#{pkt.proto})"
+        puts "#{i}. ALERT: #{desc} for #{pkt.ip_saddr} (#{pkt.proto})"
         return i+1
 end
 
-i=0
-stream.stream.each do |raw|	
-	pkt = PacketFu::Packet.parse raw
-	if (pkt.proto).include? 'TCP'
-		if null_scan? pkt
-			i = alert("NULL Scan", pkt, i)
-		end
-		if xmas_scan? pkt
-                        i = alert("XMAS Scan", pkt, i)
+def live_stream ()
+        stream = PacketFu::Capture.new(:start => true, :iface => 'eth0', :promisc => true)
+        i=0
+        stream.stream.each do |raw|
+                pkt = PacketFu::Packet.parse raw
+                if (pkt.proto).include? 'TCP'
+                        if null_scan? pkt
+                                i = alert("NULL Scan", pkt, i)
+                        end
+                        if xmas_scan? pkt
+                                i = alert("XMAS Scan", pkt, i)
+                        end
+                        if fin_scan? pkt
+                                 i = alert("FIN Scan", pkt, i)
+                        end
+
+                        if nmap_scan? pkt
+                                i = alert("NMAP Scan", pkt, i)
+                        end
+
+                        i = alert("test", pkt, i)
                 end
-		if fin_scan? pkt
-                        i = alert("FIN Scan", pkt, i)
-                end
+        end
+end
+
+
+def alert_parse (desc, line, i)
+	# split while retaining quotes
+	s = line.split(/\s(?=(?:[^"]|"[^"]*")*$)/)
+	ip_addr = s[0]
+	payload = s[5]
+	if line =~ /http/i
+		proto = "HTTP"
+	end
 		
-		if nmap_scan? pkt
-                        i = alert("NMAP Scan", pkt, i)
-                end
-			
-		i = alert("test", pkt, i)
+	puts "#{i}. ALERT: #{desc} is detected from #{ip_addr} (#{proto}) (#{payload})!"
+        return i+1
+end
+
+def phpMyAdmin? (line)
+        return line =~ /phpmyadmin/i
+end
+
+def read_log (log)
+	i=0
+	puts "#{log}"
+	f = File.new(log)
+	f.each_line do |line|
+		if phpMyAdmin? line
+			i = alert_parse("phpMyAdmin", line, i)
+		end
 	end
 end
 
+puts "#{ARGV}"
+if (ARGV.length == 0)
+	live_stream()
+else
+	read_log(ARGV[1]);
+end
